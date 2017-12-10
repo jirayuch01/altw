8a2f0bb3da9af57b7662eab373a3870a6649fdfe
jirayuch01 <jirayu_ch01@hotmail.com>
2017-12-10 23:32:27 +0700

    AltW
    AltW

--- wn_config.php
+++ wn_config.php
@@ -5,6 +5,12 @@ define("DBHOST", "localhost");
 define("DBUSER", "root");
 define("DBPW", "");
 define("DBNAME", 'wordnet');
+
+// define("DBHOST", "us-cdbr-iron-east-05.cleardb.net:3306");
+// define("DBUSER", "bcee33f3e5d0b7");
+// define("DBPW", "435837ed");
+// define("DBNAME", 'heroku_0837c81e28c2070');
+
 define("APP_HOME", "Project_I/" . basename(__DIR__));
 $sqlResources = sql(DBNAME);
 ?>
